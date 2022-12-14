40d653da8e45359e8ac97ccf62b0caa7c8860ba2
Ezisberta Kachiungo <Ezisberta.Kachiungo@emac.co.uk>
2022-12-14 17:28:32 +0000

    complete GET elephants, start GET elephant?Id

---
+++ ElephantsAzure/ElephantsAzure/Models.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.WindowsAzure.Storage.File.Protocol;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ElephantsAzure
+{
+    public class Models
+    {
+        public class Elephant
+        {
+            public Guid id { get; set; }
+            public string species { get; set; }
+            public string sex { get; set; }
+            public int dob { get; set; }
+            public int dod { get; set; }
+            public string wikiLink { get; set; }
+            public string note { get; set; }
+        }
+
+        public interface IElephantListReturner
+        {
+            public List<Elephant> ReturnElephants();
+        }
+
+        public interface ISingleElephantReturner
+        {
+            public Elephant GetElephant(string id);
+        }
+
+        public class JSonElephantListExtractor : IElephantListReturner
+        {
+            public List<Elephant> ReturnElephants()
+            {
+                string elephants = File.ReadAllText(@"..\..\..\elephants.json");
+                return JsonSerializer.Deserialize<List<Elephant>>(elephants);
+            }
+        }
+
+        public class ElephantProvider : ISingleElephantReturner
+        {
+            public List<Elephant> Elephants { get; set; }
+
+            public ElephantProvider(IElephantListReturner returner)
+            {
+                Elephants = returner.ReturnElephants();
+            }
+
+            public Elephant GetElephant(string id)
+            {
+                return Elephants.Find(elephant => elephant.id == Guid.Parse(id));
+            }
+
+        }
+
+    }
+}
